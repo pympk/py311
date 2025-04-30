8641727479491a7a84cffb080c16636a6feed7e6
pympk <yee_ping@yahoo.com>
2025-04-29 20:07:22 -0700

    a

--- stocks/notebooks/config.py
+++ stocks/notebooks/config.py
@@ -1,5 +1,5 @@
 # config.py
 # File path configuration
-date_str = '2025-04-24'  # Date in YYYY-MM-DD format
+date_str = '2025-04-25'  # Date in YYYY-MM-DD format
 DOWNLOAD_DIR = r'C:\Users\ping\Downloads'  # Raw string for Windows paths
 DEST_DIR = r'..\data'
