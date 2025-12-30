dc6b83edcc14872d7fa77ac92c8af00d3056464e
pympk <yee_ping@yahoo.com>
2025-12-30 14:41:54 -0800

    a

--- stocks/config.py
+++ stocks/config.py
@@ -4,7 +4,7 @@
 from pathlib import Path
 
 # --- File path configuration ---
-DATE_STR = '2025-12-23'
+DATE_STR = '2025-12-29'
 DOWNLOAD_DIR = Path('C:/Users/ping/Downloads')
 DEST_DIR = Path('c:/Users/ping/Files_win10/python/py311/stocks/data')
 
